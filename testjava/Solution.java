84a68a5f6b9572dc151ddf8dbdc834e5873fea61
沈明 <shenming@ebaochina.cn>
2018-11-19 15:12:32 +0800

    feat:15. 3Sum

--- testjava/Solution.java
+++ testjava/Solution.java
@@ -1,5 +1,8 @@
 import java.util.HashSet;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import com.sun.tools.javac.util.List;
 // ababcbb
 public class Solution {
   public int lengthOfLongestSubstring(String s) {
@@ -46,5 +49,31 @@ public class Solution {
       }
     }
     return -1;
+  }
+
+  public List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> res = new ArrayList<>();
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length - 2; i++) {
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+      int low = i + 1, high = nums.length - 1, sum = 0 - nums[i];
+      while (low < high) {
+        if (nums[low] + nums[high] == sum) {
+          res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+          while (low < high && nums[low] == nums[low + 1])
+            low++;
+          while (low < high && nums[high] == nums[high - 1])
+            high--;
+          low++;
+          high--;
+        } else if (nums[low] + nums[high] < sum) {
+          low++;
+        } else {
+          high--;
+        }
+      }
+    }
+    return res;
   }
 }
--- testjava/main.java
+++ testjava/main.java
@@ -1,10 +1,16 @@
+import com.sun.tools.javac.util.List;
+import java.util.List;
 class Test {
   public static void main(String[] args) {
     Solution s = new Solution();
     // int res = s.lengthOfLongestSubstring("ababcbb");
-    int[] a = {1, 2};
-    int[] b = {3, 4};
-    double res = s.findMedianSortedArrays(b, a);
+
+    // int[] a = {1, 2};
+    // int[] b = {3, 4};
+    // double res = s.findMedianSortedArrays(b, a);
+
+    int[] arr = {-1, 0, 1, 2, -1, -4};
+    List<List<Integer>> res = s.threeSum(arr);
     System.out.println(res);
   }
 }
